cb72dcd45b6851e9cd6e0b1917a89c489838763d
Laurie Waller <lauriewaller7@gmail.com>
2021-09-29 22:15:16 -0700

    remove imports

--- src/components/SurveyControl.js
+++ src/components/SurveyControl.js
@@ -1,6 +1,4 @@
 import React from "react";
-import NewSurveyForm from "./NewSurveyForm";
-import SurveyList from "./SurveyList";
 import PropTypes from "prop-types";
 import * as a from "./../actions";
 import { useSelector } from "react-redux";
